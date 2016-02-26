7c7f01fffd8a8f6af54178239847e47bab3646ff
Ke-Wei Ma <keweima@yahoo.com>
2016-02-25 19:58:59 -0500

    Added shifted cosine and made old code actually work properly

--- ch03/dft31.m
+++ ch03/dft31.m
@@ -1,24 +1,29 @@
 % dft31 Run dft on trivial time domain function
 %
 % Copyright 2013 Ke-Wei Ma
+% keweima at gmail.com
 %
 % Creates time domain data and applies DFT
-% techniques to reconstruct using Fourier.
+% techniques to reconstruct using Fourier.\
+%
 % Based on BASIC code from "Understanding the
 % FFT" by Anders E. Zonst
 
 pi2 = 2 * pi;
 k1 = pi / 8;
 k2 = 1 / pi;
-y = zeros(1,16);
-fc = zeros(1,16);
+y = zeros(1,16);    % time domain function
+
+fc = zeros(1,16);   % amplitude of derived
 fs = zeros(1,16);
-ks = zeros(1,16);
+
+kc = zeros(1,16);   % amplitudes of source
+ks = zeros(1,16);   
 
 %*****************
 %*  Prompt User  *
 %*****************
-prompt = 'Select input waveform\n1 - Cosine\n2 - Random Amplitude\n3 - "Perfect" triangle wave\n';
+prompt = 'Select input waveform\n1 - triangle \n2 - shifted cosine\n3 - "Perfect" triangle wave\n';
 choice = input(prompt);
 
 %**************************
@@ -27,7 +32,7 @@ choice = input(prompt);
 % Ideally it is not hardcoded for length but so is the book example.
 switch choice
     case 1
-        
+        % triangle
         i = 0:15;
         k3 = i * k1;
         y = cos(k3) + cos(3*k3)  ./ 9 + cos(5*k3) ./ 25 + cos(7*k3) ./49;
@@ -35,12 +40,19 @@ switch choice
         kc(i+1) = 1 ./ i.^2;        % adjust indexing because BASIC is base 0 and
                                     % Matlab is base 1.  Stupid code.
     case 2
+        % shifted cosine
+        k4 = 3 * pi/8;
+        kc(1) = cos(k4);
+        ks(1) = sin(k4);
+        i = 0:15;
+        y = cos(i*k1+k4);      
         
     case 3 
+        % perfect triangle
         k2 = pi^2/8;
         k3 = k2/4;
         i = 0:7;
-        y = k2 - k3 * i;
+        y = k2 - k3*i;
         i = 8:15;
         y(9:16) = k3 * i - 3*k2;
 end
@@ -53,8 +65,8 @@ plot(x,y);
 %**************************
 % Solve for components    *
 %**************************
-j = 0:15;
 i = 0:15;
+j = 0:15;
 fc = sum( cos(j'*i*k1) .* repmat(y',1,16)',2) ./ 16;
 fs = sum( sin(j'*i*k1) .* repmat(y',1,16)',2) ./ 16;
 
